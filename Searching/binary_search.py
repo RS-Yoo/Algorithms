426c00279b94b794099c535f598b76642450cde2
NeinShibes <76112694+NeinShibes@users.noreply.github.com>
2021-08-30 20:23:07 -0600

    binary search

---
+++ Searching/binary_search.py
@@ -0,0 +1,13 @@
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start+end) // 2
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+        return None
+
+    # if result is None, target does not exist in the array
